be2a4c4138412a32efbad6afc14390d0e0e11cf1
Ahtnezev <ahtnezev@gmail.com>
2026-04-18 18:26:39 -0600

    [A] first commit, project settings

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+
+import { Button } from './components/ui/button'
+
 import './index.css'
 
 createRoot(document.getElementById('root')!).render(
    <StrictMode>
-      <h1>Hola Pau</h1>
+      <div className='bg-gradient'>
+         <h1>Hola Pau</h1>
+         <Button>
+            Hola de new
+         </Button>
+      </div>
    </StrictMode>,
 )
