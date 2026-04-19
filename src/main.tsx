913ab8168ff59dd648bab0d183b025b5961c09ad
Ahtnezev <ahtnezev@gmail.com>
2026-04-18 18:53:10 -0600

    [A] creacion de rutas y pantallas

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
-import { Button } from './components/ui/button'
-
 import './index.css'
+import { HeroesApp } from './HeroesApp'
 
 createRoot(document.getElementById('root')!).render(
    <StrictMode>
       <div className='bg-gradient'>
-         <h1>Hola Pau</h1>
-         <Button>
-            Hola de new
-         </Button>
+         <HeroesApp />
       </div>
    </StrictMode>,
 )
