be40ad278dcad9b10daf82c7a624fcee39e47860
uneven74 <ahtnezev@gmail.com>
2026-04-18 22:51:40 -0600

    [A] add a lazy load on routes

--- src/heroes/layouts/HeroesLayout.tsx
+++ src/heroes/layouts/HeroesLayout.tsx
@@ -1,11 +1,27 @@
-import { Outlet } from "react-router"
-
+import { Link, Outlet } from "react-router"
 
 export const HeroesLayout = () => {
   return (
    <div className="bg-gradient">
-      {/* ver contenido hijo */}
-      <Outlet /> 
+      <ul>
+        <li>
+          <Link to='/'>Home</Link>
+        </li>
+        <li>
+          <Link to='/heroes/1'>Hero</Link>
+        </li>
+        <li>
+          <Link to='/search'>Búsqueda</Link>
+        </li>
+        <li>
+          <Link to='/admin'>Admin</Link>
+        </li>
+      </ul>
+
+      <section className="mt-10">
+        {/* ver contenido hijo */}
+        <Outlet /> 
+      </section>
    </div>
   )
 }
--- src/heroes/pages/search/SearchPage.tsx
+++ src/heroes/pages/search/SearchPage.tsx
@@ -1,6 +1,7 @@
-
 export const SearchPage = () => {
   return (
     <div>SearchPage</div>
   )
 }
+
+export default  SearchPage;
