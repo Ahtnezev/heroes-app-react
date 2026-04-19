10fb829984a5279df9fcc3f7bcc9bcbce5c23c44
uneven74 <ahtnezev@gmail.com>
2026-04-19 13:58:31 -0600

    [A] grid de personajes

--- src/components/ui/progress.tsx
+++ src/components/ui/progress.tsx
@@ -3,11 +3,13 @@ import { Progress as ProgressPrimitive } from "radix-ui"
 
 import { cn } from "@/lib/utils"
 
+// we add the `activeColor` field (custom) 
 function Progress({
   className,
   value,
+  activeColor = 'bg-primary',
   ...props
-}: React.ComponentProps<typeof ProgressPrimitive.Root>) {
+}: React.ComponentProps<typeof ProgressPrimitive.Root> & { activeColor?: string; }) {
   return (
     <ProgressPrimitive.Root
       data-slot="progress"
@@ -17,9 +19,10 @@ function Progress({
       )}
       {...props}
     >
+      {/* we add the active color + concatenation */}
       <ProgressPrimitive.Indicator
         data-slot="progress-indicator"
-        className="size-full flex-1 bg-primary transition-all"
+        className={ cn( activeColor, "size-full flex-1 transition-all") }
         style={{ transform: `translateX(-${100 - (value || 0)}%)` }}
       />
     </ProgressPrimitive.Root>
