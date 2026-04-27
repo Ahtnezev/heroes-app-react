cb5548f71286d7f6ddd6ec2ac154defa4ebe30d9
uneven74 <ahtnezev@gmail.com>
2026-04-26 23:24:12 -0600

    [U] manual navigation between cards

--- src/router/app.router.tsx
+++ src/router/app.router.tsx
@@ -1,5 +1,5 @@
 import { lazy } from "react";
-import { createBrowserRouter } from "react-router";
+import { createBrowserRouter, Navigate } from "react-router";
 
 import { HomePage } from '@/heroes/pages/home/HomePage';
 import { HeroPage } from "@/heroes/pages/hero/HeroPage";
@@ -23,13 +23,17 @@ export const appRouter = createBrowserRouter([
             element: <HomePage /> // || Component:
          },
          {
-            path: '/heroes/1',
+            path: '/heroes/:idSlug',
             element: <HeroPage />,
          },
          {
             path: '/search',
             element: <SearchPage />
          },
+         {
+            path: '*',
+            element: <Navigate to='/' />
+         }
       ]
    },
    {
